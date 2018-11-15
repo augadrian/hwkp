f29b10562ea4450534c63886a4958e4858562674
augadrian <augadrian@126.com>
2018-11-15 17:44:16 +0800

    11.15

--- src/main/java/com/hwkp/controller/productController.java
+++ src/main/java/com/hwkp/controller/productController.java
@@ -1,9 +1,54 @@
 package com.hwkp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hwkp.common.JsonResult;
+import com.hwkp.entity.ProductEntity;
+import com.hwkp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.io.IOException;
+
+@Controller
 public class productController {
 
+  @Autowired
+    private ProductService productService;
+
+
+
+  ObjectMapper mapToJson = new ObjectMapper();
+  JsonResult jsonResult = new JsonResult();
+
+    @ResponseBody
+    @RequestMapping(value="/addProduct", method = RequestMethod.POST)
+    public String addProduct(String productObj)throws IOException {
+      ProductEntity productEntity=mapToJson.readValue(productObj,ProductEntity.class);
+      if(productEntity.getProductId()!=null){
+        productService.save(productEntity);
+        jsonResult.setStatus(200);
+        jsonResult.setMessage("添加成功");
+        jsonResult.setData(productEntity);
+      }else {
+        jsonResult.setStatus(500);
+        jsonResult.setMessage("添加失败");
+      }
+      return mapToJson.writeValueAsString(jsonResult);
+    }
+
+    @ResponseBody
+    @RequestMapping("/delProduct")
+    public String delProduct(Integer id){
+      return null;
+    }
+
+    @ResponseBody
+    @RequestMapping("/updateProduct")
+    public String updateProduct(ProductEntity productEntity){
+      return null;
+    }
 
 }
--- src/main/java/com/hwkp/service/ProductService.java
+++ src/main/java/com/hwkp/service/ProductService.java
@@ -1,4 +1,18 @@
 package com.hwkp.service;
 
-public class ProductService {
+
+import com.hwkp.entity.ProductEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public interface ProductService  {
+
+    void save(ProductEntity productEntity);
+    void update(ProductEntity productEntity);
+    void delete(ProductEntity productEntity);
+    ProductEntity findById(ProductEntity productEntity);
+    List<ProductEntity> findAll(Integer pageNo, Integer pageSize, Map<String,Object> params);
 }
